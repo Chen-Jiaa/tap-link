fa5258115e8bf5560dd465e6cb896d8082590c71
Chen-Jiaa <eongchenjia@gmail.com>
2025-06-24 23:04:12 +0800

    added console log

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -15,25 +15,32 @@ const cache = new Map<string, CacheEntry>()
 
 export default async function TapPage() {
 
+  console.time('total-function')
+  console.time('supabase-connection')
+
   const supabase = await createClient()
+  console.timeEnd('supabase-connection')
 
   const cached = cache.get('url')
-
   if (cached && Date.now() - cached.timestamp < 5000) {
+    console.log('Using cache - should be instant')
     redirect(cached.url)
   }
 
+  console.time('supabase-query')
   const { data, error } = await supabase
     .from('current_segment')
     .select('url')
     .eq('is_active', true)
     .single<CurrentSegmentRow>()
+  console.timeEnd('supabase-query')
 
   if (error || !data.url) {
     throw new Error('Redirect URL not found.')
   }
 
   cache.set('url', { timestamp: Date.now(), url: data.url })
+  console.timeEnd('total-function')
   redirect(data.url)
 
 }
